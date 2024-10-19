1916965c3c027658f84cdea74f88368535341a56
muhamdAfzal <afzalur.rh@gmail.com>
2024-10-19 21:10:10 +0600

    student_result #task-2 complete

--- student_result/index.php
+++ student_result/index.php
@@ -1,62 +1,72 @@
 <?php
 function studentMarks($marks)
 {
+  $output = '';
 
-  //___ Mark Range Validation
   for ($i = 0; $i < count($marks); $i++) {
 
-    if ($marks[$i] <= 0 || $marks[$i] >= 100) {
-      echo 'Invalid Marks';
+
+    //___ Mark Range Validation
+    if ($marks[$i] < 0 || $marks[$i] > 100) {
+      echo 'Mark range is invalid';
       return;
     }
+
+
+    //___ Fail Condition
+    if ($marks[$i] < 33) {
+      $output = "Student Failed!";
+      return $output;
+    }
   }
+
 
   $totalMarks = array_sum($marks);  // Total Marks
 
   $averageMark = $totalMarks / count($marks); // Average Marks
-  echo $averageMark;
-  echo "</br >";
 
 
   $grade = ' ';
 
   //___ Grade Calculation 
   switch ($averageMark) {
-    case ($averageMark >= 80 && $averageMark <= 100):
+    case $averageMark >= 80:
       $grade = 'A+';
       break;
 
-    case ($averageMark >= 70 && $averageMark <= 79):
+    case $averageMark >= 70:
       $grade = 'A';
       break;
 
-    case ($averageMark >= 60 && $averageMark <= 69):
+    case $averageMark >= 60:
       $grade = 'A-';
       break;
 
-    case ($averageMark >= 50 && $averageMark <= 59):
+    case $averageMark >= 50:
       $grade = 'B';
       break;
 
-    case ($averageMark >= 40 && $averageMark <= 49):
+    case $averageMark >= 40:
       $grade = 'C';
       break;
 
-    case ($averageMark >= 33 && $averageMark <= 39):
+    case $averageMark >= 33:
       $grade = 'D';
-      break;
-
-      // case $averageMark < 33:
-    default:
-      $grade = 'F';
   };
 
-  echo "Grade is $grade";
-  echo "</br >";
 
   //___ Returning Final Result Output
   $output = ($grade == 'F') ? "Student Failed" : "Total Marks: $totalMarks </br > Average Mark: $averageMark </br > Grade: $grade";
+
   return $output;
 };
 
-echo studentMarks([40, 35, 87, 44, 90]);
+
+
+echo studentMarks([51, 99, 88, 73, -19]);
+echo "</br >";
+
+echo studentMarks([38, 35, 17, 34, 50]);
+echo "</br >";
+
+echo studentMarks([90, 44, 90, 65, 39]);
